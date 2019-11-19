76584a54df7c587b906bb8394d0a9cf9331f6f5b
alevko55555 <alevko55555@gmail.com>
2019-11-19 18:37:26 +0300

    Scripted auto-commit on change (2019-11-19 18:37:26) by gitwatch.sh

--- lab/src/main/java/lab4/MessageWhithResultOfOneTest.java
+++ lab/src/main/java/lab4/MessageWhithResultOfOneTest.java
@@ -1,4 +1,19 @@
 package lab4;
 
 public class MessageWhithResultOfOneTest {
+    private final long packageId;
+    private final String result;
+
+    public MessageWhithResultOfOneTest(long packageId, String result) {
+        this.packageId = packageId;
+        this.result = result;
+    }
+
+    public long getPackageId() {
+        return packageId;
+    }
+
+    public String getResult() {
+        return result;
+    }
 }
