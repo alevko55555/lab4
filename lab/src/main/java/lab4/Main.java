1b4d90a0e84dbec4a7ecf5bc473c5018b8cf4137
alevko55555 <alevko55555@gmail.com>
2019-11-19 15:58:08 +0300

    Scripted auto-commit on change (2019-11-19 15:58:08) by gitwatch.sh

--- lab/src/main/java/lab4/Main.java
+++ lab/src/main/java/lab4/Main.java
@@ -7,12 +7,14 @@ import akka.actor.Props;
 import akka.http.javadsl.server.AllDirectives;
 
 public class Main extends AllDirectives {
-    ActorSystem system = ActorSystem.create("test");
-    ActorRef storeActor = system.actorOf(
-            Props.create(StoreActor.class)
-    );
-    storeActor.tell(
-        new StoreActor.StoreMessage("test", "test"),
-        ActorRef.noSender()
+    public static void main(){
+        ActorSystem system = ActorSystem.create("test");
+        ActorRef storeActor = system.actorOf(
+                Props.create(StoreActor.class)
         );
+        storeActor.tell(
+                new StoreActor.StoreMessage("test", "test"),
+                ActorRef.noSender()
+        );
+    }
 }
