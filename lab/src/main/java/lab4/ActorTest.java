c0125bf31508c097754bf674f64ad4f1a850eb3f
alevko55555 <alevko55555@gmail.com>
2019-11-19 17:32:45 +0300

    Scripted auto-commit on change (2019-11-19 17:32:45) by gitwatch.sh

--- lab/src/main/java/lab4/ActorTest.java
+++ lab/src/main/java/lab4/ActorTest.java
@@ -1,10 +1,12 @@
 package lab4;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ActorTest {
     private final String testName;
     private final String expectedResult;
     private final Object[] params;
-    public ActorTest(String testName, String expectedResult, Object[] params) {
+    public ActorTest(@JsonProperty("testName") String testName, @JsonProperty("expectedResult") String expectedResult, @JsonProperty("params") Object[] params) {
         this.testName = testName;
         this.expectedResult = expectedResult;
         this.params = params;
--- lab/src/main/java/lab4/ActortResult.java
+++ lab/src/main/java/lab4/ActortResult.java
@@ -3,4 +3,8 @@ package lab4;
 public class ActortResult {
     private final long packageId;
     private final String result;
+    public ActortResult(long packageId, String result){
+        this.packageId = packageId;
+        this.result = result;
+    }
 }
