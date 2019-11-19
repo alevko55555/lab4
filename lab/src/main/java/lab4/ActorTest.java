49466749673fcaa2297149c6a04c02f4258f4688
alevko55555 <alevko55555@gmail.com>
2019-11-19 17:25:24 +0300

    Scripted auto-commit on change (2019-11-19 17:25:24) by gitwatch.sh

--- lab/src/main/java/lab4/ActorTest.java
+++ lab/src/main/java/lab4/ActorTest.java
@@ -3,7 +3,12 @@ package lab4;
 public class ActorTest {
     private final String testName;
     private final String expectedResult;
-    private final Ob params;
+    private final Object[] params;
+    public ActorTest {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
     /*
     {"testName":"test1",
 "expectedResult":"2.0",
