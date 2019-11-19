64dc7f0fe4523e853a4e9bf5a53fddccc5c9e65f
alevko55555 <alevko55555@gmail.com>
2019-11-19 15:58:44 +0300

    Scripted auto-commit on change (2019-11-19 15:58:44) by gitwatch.sh

--- lab/src/main/java/lab4/Main.java
+++ lab/src/main/java/lab4/Main.java
@@ -6,8 +6,10 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.server.AllDirectives;
 
+import java.io.IOException;
+
 public class Main extends AllDirectives {
-    public static void main(){
+    public static void main(String [] args) throws IOException {
         ActorSystem system = ActorSystem.create("test");
         ActorRef storeActor = system.actorOf(
                 Props.create(StoreActor.class)
