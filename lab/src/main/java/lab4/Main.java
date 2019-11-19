5c5cf487e333e32830e12f90629107f991127fb9
alevko55555 <alevko55555@gmail.com>
2019-11-19 15:51:58 +0300

    Scripted auto-commit on change (2019-11-19 15:51:58) by gitwatch.sh

--- lab/src/main/java/lab4/Main.java
+++ lab/src/main/java/lab4/Main.java
@@ -2,4 +2,11 @@ package lab4;
 
 
 public class Main {
+    ActorSystem system = ActorSystem.create("test");
+    ActorRef storeActor = system.actorOf(
+    );
+storeActor.tell(
+        new StoreActor.StoreMessage("test", "test"),
+        ActorRef.noSender()
+        );
 }
