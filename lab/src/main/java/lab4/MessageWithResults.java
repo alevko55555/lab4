340423e4d66ad789eebc8270e4b88afedb4d024e
alevko55555 <alevko55555@gmail.com>
2019-11-19 18:44:40 +0300

    Scripted auto-commit on change (2019-11-19 18:44:40) by gitwatch.sh

--- lab/src/main/java/lab4/MessageWithResults.java
+++ lab/src/main/java/lab4/MessageWithResults.java
@@ -1,4 +1,14 @@
 package lab4;
 
 public class MessageWithResults {
+    private final long packageId;
+    private final String jsScript;
+    private final String functionName;
+    private final Test[] tests;
+    public MessageWithResults(long packageId, String jsScript, String functionName, Test[] tests){
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
 }
