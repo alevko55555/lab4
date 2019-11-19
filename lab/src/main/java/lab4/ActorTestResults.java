81153d2a9784b70764e8af767b5397e55a7bd783
alevko55555 <alevko55555@gmail.com>
2019-11-19 17:10:03 +0300

    Scripted auto-commit on change (2019-11-19 17:10:03) by gitwatch.sh

--- lab/src/main/java/lab4/ActorTestResults.java
+++ lab/src/main/java/lab4/ActorTestResults.java
@@ -1,4 +1,16 @@
 package lab4;
 
 public class ActorTestResults {
+    private final long packageId;
+    private final List<> results;
+    public ActorTestResults {
+        this.packageId = packageId;
+        this.results = results;
+    }
+    public long getPackageId() {
+        return packageId;
+    }
+    public List<> getResults() {
+        return results;
+    }
 }
