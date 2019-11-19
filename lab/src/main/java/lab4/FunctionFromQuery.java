803eb1d50b984e1cd8c080e2128243c534dd9931
alevko55555 <alevko55555@gmail.com>
2019-11-19 17:41:46 +0300

    Scripted auto-commit on change (2019-11-19 17:41:46) by gitwatch.sh

--- lab/src/main/java/lab4/FunctionFromQuery.java
+++ lab/src/main/java/lab4/FunctionFromQuery.java
@@ -1,4 +1,10 @@
 package lab4;
 
 public class FunctionFromQuery {
+
+    /*
+    "packageId":"11",
+"jsScript":"var divideFn = function(a,b) { return a/b} ",
+"functionName":"divideFn",
+     */
 }
