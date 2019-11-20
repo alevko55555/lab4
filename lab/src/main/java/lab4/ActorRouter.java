ff836fdbb10186bb7c6831c5b5c0dbe1918e8662
alevko55555 <alevko55555@gmail.com>
2019-11-20 19:49:13 +0300

    Scripted auto-commit on change (2019-11-20 19:49:13) by gitwatch.sh

--- lab/src/main/java/lab4/ActorRouter.java
+++ lab/src/main/java/lab4/ActorRouter.java
@@ -4,5 +4,6 @@ import akka.actor.AbstractActor;
 import akka.actor.Props;
 
 public class ActorRouter extends AbstractActor {
-    Props props1 = Props.create(ActorRouter.class, Act);
+    Props props1 = Props.create(ActorRouter.class);
+    
 }
