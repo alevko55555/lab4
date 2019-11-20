e390ab3a2b55a29b0e940028fba25ed8622dc878
alevko55555 <alevko55555@gmail.com>
2019-11-20 20:01:54 +0300

    Scripted auto-commit on change (2019-11-20 20:01:54) by gitwatch.sh

--- lab/src/main/java/lab4/ActorRouter.java
+++ lab/src/main/java/lab4/ActorRouter.java
@@ -5,10 +5,11 @@ import akka.actor.ActorRef;
 import akka.actor.Props;
 
 public class ActorRouter extends AbstractActor {
-    public
-    final ActorRef router;
+    private final ActorRef storageActor;
+    private final ActorRef router;
 
-    public ActorRouter (ActorRef router) {
+    public ActorRouter (ActorRef storageActor, ActorRef router) {
+        this.storageActor = getContext().actorOf(Props.create(ActorStorage.class));
         this.router = getContext().actorOf(Props.create(ActorRouter.class));
     }
 }
