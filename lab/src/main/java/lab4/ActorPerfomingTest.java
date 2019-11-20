dfa6285115ea4a0168f31902de8b1fe7e8c0e276
alevko55555 <alevko55555@gmail.com>
2019-11-20 20:10:21 +0300

    Scripted auto-commit on change (2019-11-20 20:10:21) by gitwatch.sh

--- lab/src/main/java/lab4/ActorPerfomingTest.java
+++ lab/src/main/java/lab4/ActorPerfomingTest.java
@@ -3,4 +3,8 @@ package lab4;
 import akka.actor.AbstractActor;
 
 public class ActorPerfomingTest extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
