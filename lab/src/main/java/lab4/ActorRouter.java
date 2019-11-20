b2a16685e940ca9270f208e64aaa8673455e4f8c
alevko55555 <alevko55555@gmail.com>
2019-11-20 21:00:53 +0300

    Scripted auto-commit on change (2019-11-20 21:00:53) by gitwatch.sh

--- lab/src/main/java/lab4/ActorRouter.java
+++ lab/src/main/java/lab4/ActorRouter.java
@@ -21,7 +21,7 @@ public class ActorRouter extends AbstractActor {
                 .match(FunctionFromQuery.class, msg -> {
                     int len = msg.getTests().length;
                     for (int i = 0; i < len; i++){
-                        router.tell(new MessageWithResults(msg.getPackageId(), msg.getJsScript(), msg.getFunctionName(), ), storageActor);
+                        router.tell(new MessageWithTest(msg.getPackageId(), msg.getJsScript(), msg.getFunctionName(), test -> ), storageActor);
                     }
                 })
                 .build();
--- lab/src/main/java/lab4/MessageWithTest.java
+++ lab/src/main/java/lab4/MessageWithTest.java
@@ -1,11 +1,11 @@
 package lab4;
 
-public class MessageWithResults {
+public class MessageWithTest {
     private final long packageId;
     private final String jsScript;
     private final String functionName;
     private final Test[] tests;
-    public MessageWithResults(long packageId, String jsScript, String functionName, Test[] tests){
+    public MessageWithTest(long packageId, String jsScript, String functionName, Test[] tests){
         this.packageId = packageId;
         this.jsScript = jsScript;
         this.functionName = functionName;
