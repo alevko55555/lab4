076b09178c9b57fd83c782ffe7aad4dfcd4b0d4f
alevko55555 <alevko55555@gmail.com>
2019-11-21 10:38:15 +0300

    Scripted auto-commit on change (2019-11-21 10:38:14) by gitwatch.sh

--- lab/src/main/java/lab4/ActorStorage.java
+++ lab/src/main/java/lab4/ActorStorage.java
@@ -3,6 +3,8 @@ package lab4;
 import akka.actor.AbstractActor;
 
 public class ActorStorage extends AbstractActor {
+    private Map
+
     @Override
     public Receive createReceive() {
         return null;
