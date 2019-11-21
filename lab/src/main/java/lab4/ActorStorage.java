11e252857613ac7af9c7315fe1e83120a5fc3ad9
alevko55555 <alevko55555@gmail.com>
2019-11-21 10:41:11 +0300

    Scripted auto-commit on change (2019-11-21 10:41:11) by gitwatch.sh

--- lab/src/main/java/lab4/ActorStorage.java
+++ lab/src/main/java/lab4/ActorStorage.java
@@ -4,7 +4,7 @@ import akka.actor.AbstractActor;
 import scala.collection.immutable.List;
 
 public class ActorStorage extends AbstractActor {
-    private final Map<long, List<Result>> storage;
+    private final Map<Long, List<Result>> storage;
 
     @Override
     public Receive createReceive() {
