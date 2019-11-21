758d92d73f5f69b1e21cad323c1548b1d9ec8424
alevko55555 <alevko55555@gmail.com>
2019-11-21 12:29:37 +0300

    Scripted auto-commit on change (2019-11-21 12:29:37) by gitwatch.sh

--- lab4_1/src/main/java/lab4/ActorStorage.java
+++ lab4_1/src/main/java/lab4/ActorStorage.java
@@ -10,21 +10,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ActorStorage extends AbstractActor {
-    private HashMap<Long, List<Result>> storage = new HashMap<>();
+    private HashMap<Long, ArrayList<MessageWithTest>> storage = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .create()
-                .match(RequestMessageOfPackageTestResult.class,
-                        require -> getSender().tell(
-                                storage.get(require.getPackageId()).toList(),
-                                ActorRef.noSender()
-                        ))
-                .match(MessageWithTest.class,
-                        msg -> getSender().tell(
-                                new TestResults(msg.getPackageId(), storage.get(msg.getPackageId())),
-                                ActorRef.noSender()
-                        ));
+                .match()
+                .match();
     }
 }
