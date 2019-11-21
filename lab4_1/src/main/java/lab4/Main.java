c8bf042128db2b2c9ee6c2b2bc856a3c9a6b189e
alevko55555 <alevko55555@gmail.com>
2019-11-21 11:23:59 +0300

    Scripted auto-commit on change (2019-11-21 11:23:59) by gitwatch.sh

--- lab4_1/src/main/java/lab4/Main.java
+++ lab4_1/src/main/java/lab4/Main.java
@@ -1,2 +1,37 @@
-public class Main {
+package lab4;
+
+
+public class Main extends AllDirectives {
+    private static final int SERVER_PORT =  8080;
+
+    public static void main(String[] args) throws IOException {
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        Main instance = new Main();
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", SERVER_PORT),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+    }
+
+    private Route createRoute(ActorSystem system) {
+        ActorRef storeActor = system.actorOf(
+                Props.create(ActorRouter.class)
+        );
+        /*
+        storeActor.tell(
+                new ActorStorage().StoreMessage("test", "test"),
+                ActorRef.noSender()
+        );*/
+        return null;
+    }
 }
